15c62b3a1b872a49d1b57fd670ad86e9a5d07080
SpongeBob_Han <1605319671@qq.com>
2015-05-11 23:16:55 +0800

    init

---
+++ MBTip/MBTip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MBTip
+//
+//  Created by Bing Ma on 5/11/15.
+//  Copyright (c) 2015 SpongeBob-GitHub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MBTip/MBTip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MBTip
+//
+//  Created by Bing Ma on 5/11/15.
+//  Copyright (c) 2015 SpongeBob-GitHub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
